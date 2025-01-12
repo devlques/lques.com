62852a89b4cf2f746a6b87a44c78590720fa15e2
Luis Quesada <lquesapp@gmail.com>
2025-01-12 00:03:12 -0600

    Career: improvements to modal mobile view and implemented Download CV button

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -25,25 +25,33 @@ export default function Modal({
     openStatus && (
       <div
         className=" overflow-y-scroll sm:overflow-y-hidden
-      p-8 absolute h-full w-3/4 flex
+      p-2 sm:p-8 absolute h-full w-3/4 flex
       flex-col sm:flex-row self-center justify-between 
       border-8 bg-black border-double rounded-3xl 
       slide-from-top gap-4 z-40
     "
       >
-        <div className="flex sm:basis-1/4 self-center sm:self-end order-last sm:order-first">
+        <div className="flex flex-col items-center self-center sm:basis-2/8 sm:self-end order-last sm:order-first gap-2">
+           <a
+              href=""
+              target="_blank"
+              className="text-2xl font-bold text-nowrap p-2 border-2 border-white rounded-2xl
+                hover:bg-white hover:text-black hover:outline hover:outline-offset-4 hover:outline-white"
+             > DOWNLOAD CV
+          </a>
           <Image
-            alt="duck pixel art"
+            className="sm:w-36"
+            alt="duck pixel art "
             src="/happyDogNoBG.png"
-            width={150}
-            height={150}
+            width={100}
+            height={100}
             unoptimized
           />
         </div>
 
         <div
           className="
-          basis-3/4 border-4 border-white border-double
+          basis-5/8 border-4 border-white border-double
           text-gray-400 rounded-3xl
           p-4 overflow-y-auto
         "
@@ -84,20 +92,20 @@ export default function Modal({
             );
           })}
         </div>
-        <div className="flex flex-col items-center order-first sm:order-last">
+        <div className="flex flex-col basis-1/8 items-center order-first sm:order-last">
           <button
             type="button"
             onClick={() => setOpenStatus(false)}
           >
             <IoCloseOutline
-              className="sm:hidden
+              className="sm:hidden size-12 
                 bg-white rounded-full h-fit cursor-pointer
                 hover:bg-black hover:text-white hover:border-white text-black
                 hover:outline hover:outline-offset-2 hover:outline-white"
-              size={60}
             />
             <div
-              className="text-4xl px-2 hidden sm:block hover:border-white rounded-2xl
+              className="text-4xl font-bold px-2 hidden sm:block rounded-2xl
+                hover:bg-white hover:text-black border-2 border-white
                 hover:outline hover:outline-offset-2 hover:outline-white"
             >
               ESC
